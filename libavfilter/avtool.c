5bc04de31996ee9c760d23dafd576ba35a8a97bc
psi <ledyba@users.sourceforge.jp>
2013-02-10 20:31:54 +0900

    logを追加

--- libavfilter/avtool.c
+++ libavfilter/avtool.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include "common/framehook_ext.h"
 #include "avtool.h"
+#include "../libavutil/log.h"
 
 static toolbox Box = {
 	.version = TOOLBOX_VERSION,
@@ -20,10 +21,10 @@ static toolbox Box = {
 /* こちらはffmpeg側から呼ばれる関数 */
 
 int tool_registerInfo(int64_t duration,int64_t rec_time){
-	if(duration > rec_time && rec_time > 0){
-		Box.video_length = ((double)rec_time) / AV_TIME_BASE;
-	}
-	Box.video_length = ((double)duration)/AV_TIME_BASE;
+	double const dur = ((double)duration)/AV_TIME_BASE;
+	double const rectime = ((double)rec_time)/AV_TIME_BASE;
+	Box.video_length = (dur > rectime && rectime > 0) ? rectime : dur;
+    av_log(NULL, AV_LOG_DEBUG, "<info for Saccubus registered> duration: %f rectime: %f => len: %f\n", dur, rectime, Box.video_length);
 	return 0;
 }
 
